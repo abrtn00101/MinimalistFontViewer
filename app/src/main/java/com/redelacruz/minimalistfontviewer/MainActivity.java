754fdb253b629a9f2e21e6e771d051d1462a0453
Robert Ernest Sulapas dela Cruz <r.e.delacruz@gmail.com>
2017-05-17 14:02:33 +0800

    immersive fullscreen, removed back-compat to 16 API < 19, layout unstable

--- app/src/main/java/com/redelacruz/minimalistfontviewer/MainActivity.java
+++ app/src/main/java/com/redelacruz/minimalistfontviewer/MainActivity.java
@@ -2,9 +2,9 @@ package com.redelacruz.minimalistfontviewer;
 
 import android.annotation.TargetApi;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,14 +24,11 @@ public class MainActivity extends AppCompatActivity {
 
         final Button test_button = (Button) findViewById(R.id.test_button);
         test_button.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 setFullscreen(!isFullscreen);
             }
         });
-
-        Log.d(TAG, "onCreate");
     }
 
     @Override
@@ -40,16 +37,12 @@ public class MainActivity extends AppCompatActivity {
         if (hasFocus) {
             updateFullscreen();
         }
-
-        Log.d(TAG, "onWindowFocusChanged");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         updateFullscreen();
-
-        Log.d(TAG, "onResume");
     }
 
     public void setFullscreen(boolean fullscreen) {
@@ -59,48 +52,45 @@ public class MainActivity extends AppCompatActivity {
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
     private void updateFullscreen() {
-        Log.d(TAG, "updateFullscreen");
-
+        //TODO: Fix unstable pre-KitKat layout
+        int uiOptions = 0;
         View decorView = getWindow().getDecorView();
         boolean isUpdateNeeded = isFullscreen != ((decorView.getSystemUiVisibility() & View.SYSTEM_UI_FLAG_FULLSCREEN) == View.SYSTEM_UI_FLAG_FULLSCREEN);
-
-        Log.d(TAG, "isUpdatedNeeded = " + isUpdateNeeded);
-        Log.d(TAG, "isFullscreen = " + isFullscreen);
-        Log.d(TAG, "system flag is not set = " + ((decorView.getSystemUiVisibility() & View.SYSTEM_UI_FLAG_FULLSCREEN) != View.SYSTEM_UI_FLAG_FULLSCREEN));
 
         if (isUpdateNeeded) {
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
                 ActionBar actionBar = getSupportActionBar();
 
                 if (isFullscreen) {
-                    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
-                    actionBar.hide();
+                    uiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+                    if (actionBar != null) {
+                        actionBar.hide();
+                    }
                     Log.d(TAG, "Fullscreen API < 19");
                 } else {
-                    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-                    actionBar.show();
+                    uiOptions ^= View.SYSTEM_UI_FLAG_VISIBLE;
+                    if (actionBar != null) {
+                        actionBar.show();
+                    }
                     Log.d(TAG, "Not fullscreen API < 19");
                 }
             } else {
                 if (isFullscreen) {
-                    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    uiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-                    Log.d(TAG, "Fullscreen API => 19");
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                    Log.d(TAG, "Fullscreen API >= 19");
                 } else {
-                    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-                    Log.d(TAG, "Not fullscreen API => 19");
+                    uiOptions ^= View.SYSTEM_UI_FLAG_VISIBLE;
+                    Log.d(TAG, "Not fullscreen API >= 19");
                 }
             }
         }
+
+        uiOptions ^= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
